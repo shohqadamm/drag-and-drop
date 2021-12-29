4c95662d7c0fa313f8df1a8873613ac675db853b
Shohqadamm <ergashevshohqadam@gmail.com>
2021-12-29 13:31:34 +0500

    make it better

---
+++ src/components/DragZone.vue
@@ -0,0 +1,68 @@
+<template>
+  <div
+    
+    @dragenter.prevent="toggleActive"
+    @dragleave.prevent="toggleActive"
+    @dragover.prevent
+    @drop.prevent="toggleActive"
+    :class="{'activeDropZone':active}"
+    class="dragzone"
+  >
+    <span>Drag or Drop file</span>
+    <span>OR</span>
+    <label for="dragzoneFile">Select file</label>
+    <input type="file" id="dragzoneFile" class="dropzoneFile" />
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  name: "DragZone",
+  props: {
+    msg: String,
+  },
+  setup(){
+      const active = ref(false);
+      const toggleActive = ()=>{
+          active.value = !active.value
+      };
+      return{active, toggleActive}
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.dragzone {
+  width: 400px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  border: 2px dashed #41b883;
+  background: #fff;
+  transition: 0.3s ease all;
+  grid-gap: 20px;
+}
+label {
+  padding: 8px 15px;
+  color: #fff;
+  transition: 0.2s ease all;
+  background: #41b883;
+}
+input {
+  display: none;
+}
+.activeDropZone{
+    background: #41b883;
+}
+.activeDropZone>*{
+    color: #fff;
+}.activeDropZone>label{
+    background: #fff;
+    color: #41b882;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <h1>Drop DragZone</h1>
+    <DragZone @drop.prevent="drop" @change="selectedFile" msg="Welcome to Your Vue.js App" />
+    <span class="file-info">File: {{dropzoneFile.name}}</span>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import DragZone from "@/components/DragZone.vue";
+import {ref} from 'vue'
+export default {
+  name: "Home",
+  components: {
+    DragZone,
+  },
+  setup(){
+    let dropzoneFile = ref("");
+    const drop = (e) => {
+      dropzoneFile.value = e.dataTransfer.files[0];
+    };
+     const selectedFile = () => {
+      dropzoneFile.value = document.querySelector(".dropzoneFile").files[0];
+    };
+    return {dropzoneFile, drop, selectedFile};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.home{
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+h1, span{
+  margin:  20px 0;
+}
+
+</style>
